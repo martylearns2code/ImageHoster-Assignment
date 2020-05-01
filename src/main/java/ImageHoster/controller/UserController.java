4484539a49124fc8d3706b8717e1558c12bcf710
moorthybasava <moorthybasava@hotmail.com>
2020-05-01 16:22:09 +0530

    Fourth Commit---commit to the add-features branch---first feature--to validate the password entered by the user to register---change implement--UserController,UserService files and users/registration.html file modified

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -40,9 +40,17 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user,Model model) {
+        boolean validPassword = userService.validatePassword(user.getPassword());
+        if(validPassword) {
+            userService.registerUser(user);
+            return "redirect:/users/login";
+        }else{
+            String passwordTypeError = "Password must contain at least 1 alphabet, 1 number & 1 special character";
+            model.addAttribute("User", user);
+            model.addAttribute("passwordTypeError",passwordTypeError);
+            return "users/registration";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
